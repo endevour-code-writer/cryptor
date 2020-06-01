75d83433fc06d56715ecffaae13ddda501c445ef
endevour <ivanudedu@gmail.com>
2020-06-01 12:08:00 +0300

    add flat flag using (encode vowel)

--- internal/stringCryptor/cryptor.go
+++ internal/stringCryptor/cryptor.go
@@ -1,13 +1,41 @@
 package stringCryptor
 
-import "strings"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 const (
 	PigLatinTail = "ay"
 	Vowels = "aeiou"
 )
 
-func ToPigLatin(str string) string  {
+func Crypt(args []string) {
+	if 0 == len(args) {
+		fmt.Println("No String to crypt")
+		os.Exit(0)
+	}
+
+	flag := args[:1][0]
+	sourceToCrypt := args[1:]
+
+	if 0 == len(sourceToCrypt) {
+		fmt.Println("No String to crypt")
+		os.Exit(0)
+	}
+
+	strToCrypt := strings.Join(args[1:], " ")
+
+	switch flag {
+		case "toPigLatin":
+		fmt.Println(toPigLatin(strToCrypt))
+	default:
+		fmt.Println("Too far away.")
+	}
+}
+
+func toPigLatin(str string) string  {
 	var encoded []string
 	words := strings.Split(str, " ")
 
